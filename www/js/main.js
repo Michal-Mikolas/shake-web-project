46d5dd070ec782ab4b56e65096d12fba478f1bbe
Michal Mikolas <nanuqcz@gmail.com>
2013-05-12 15:12:51 +0200

    Added data-confirm-question javascript support.

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,14 @@
-$(function(){
+jQuery(function(){
+
+	/*
+	 * Confirm Question
+	 */
+	jQuery('[data-confirm-question]').click(function(){
+		var $this = jQuery(this);
+		var question = $this.data('confirm-question') || 'Really delete?';
+
+		if (!confirm(question))
+			return false;
+	});
 
 });
