bc6cc3ab8941282d18b80b2e9b48a88c8a1ffc47
Michal Mikolas <nanuqcz@gmail.com>
2014-08-27 22:03:53 +0200

    main.js: Added support for CKEditor & TW Bootstrap 'last active tab' memory.

--- www/js/main.js
+++ www/js/main.js
@@ -11,4 +11,49 @@ jQuery(function(){
 			return false;
 	});
 
+
+	/*
+	 * CKEditor 
+	 */
+	if (jQuery.fn.ckeditor) {
+		jQuery('textarea[name$=_html]').ckeditor({
+			toolbarGroups: [
+				{ name: 'document',    groups: [ 'mode', 'document', 'doctools' ] },
+				{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+				{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+				//{ name: 'forms' },
+				'/',
+				{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+				{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+				{ name: 'links' },
+				{ name: 'insert' },
+				'/',
+				{ name: 'styles' },  // style, format, font-family & font-size
+				//{ name: 'colors' },
+				{ name: 'tools' },
+				{ name: 'others' },
+				{ name: 'about' }
+			],
+			removeButtons: 'Font,FontSize,CreateDiv',
+			allowedContent: true  // true - prevent CKEditor to clearing disallowed tags
+		});		
+	}
+
+
+	/*
+	 * TW Bootstrap Tabs
+	 */
+	$('a[data-toggle="tab"]').on('click', function (e) {
+		//save the latest tab; use cookies if you like 'em better:
+		localStorage.setItem('lastTab', $(e.target).attr('href'));
+	});
+	//go to the latest tab, if it exists:
+	var lastTab = localStorage.getItem('lastTab');
+	if (lastTab) {
+		$('a[href="'+lastTab+'"]').click();
+	}
+
+	jQuery('html.js ul.nav-tabs, html.js .tab-content').show();
+	/* --- End TW Bootstrap Tabs --- */
+
 });
